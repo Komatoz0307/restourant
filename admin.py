99d7dca0683aba941b3c4ba3ac497af28b9b593c
Komatoz0307 <3leachim@gmail.com>
2022-07-10 15:06:34 +0300

    test commit

--- admin.py
+++ admin.py
@@ -4,7 +4,7 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget
 import sqlite3
 
-
+# hello
 # Класс Welcome "welcome_admin.ui"
 class Welcome(QDialog): 
 	def __init__(self):
